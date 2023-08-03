d7a88d32ab7abc8b89a4149824cb24ed678ce23a
npolyak <nick.polyak@verizon.net>
2023-08-03 00:13:50 -0400

    added a sample

---
+++ MissingWinUIFeatures/NP.Demos.GenericTypesInXaml/NP.Demos.GenericTypesInXaml/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace NP.Demos.GenericTypesInXaml.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MissingWinUIFeatures/NP.Demos.GenericTypesInXaml/NP.Demos.GenericTypesInXaml/Views/MyGenericClass.cs
@@ -0,0 +1,9 @@
+﻿namespace NP.Demos.GenericTypesInXaml.Views
+{
+    public class MyGenericClass<T1, T2>
+    {
+        public T1? Val1 { get; set; }
+
+        public T2? Val2 { get; set; }
+    }
+}
