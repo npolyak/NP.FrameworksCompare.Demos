377a8886873cf9c4ffe6365b8bbb35048a32ce60
npolyak <nick.polyak@verizon.net>
2023-08-06 23:08:37 -0400

    added binding Sample

---
+++ AvaloniaFeaturesMissingUnoAndMAUI/NP.Demos.BindingSample/NP.Demos.BindingSample/Views/BoolAndMultiValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace NP.Demos.BindingSample.Views
+{
+    public class BoolAndMultiValueConverter : IMultiValueConverter
+    {
+        public static BoolAndMultiValueConverter Instance { get; } = new BoolAndMultiValueConverter();
+
+        public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            foreach(var item in values)
+            {
+                if (item is bool b) 
+                {
+                    if (!b)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
