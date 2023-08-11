ec85d286df5e1147c3bd308b5936e973138bca62
npolyak <nick.polyak@verizon.net>
2023-08-10 20:22:21 -0400

    added a comment, removed some prototyping code

--- AvaloniaFeaturesMissingUnoAndMAUI/NP.Demos.ElementToImageSample/NP.Demos.ElementToImageSample/Views/MainView.axaml.cs
+++ AvaloniaFeaturesMissingUnoAndMAUI/NP.Demos.ElementToImageSample/NP.Demos.ElementToImageSample/Views/MainView.axaml.cs
@@ -25,21 +25,11 @@ public partial class MainView : UserControl
             new RenderTargetBitmap(pixSize, dpi);
         var size = new Size(ThePanel.Bounds.Width, ThePanel.Bounds.Height);
 
-        ThePanel.Measure(size);
-        //this.Arrange(new Rect(new Size(Bounds.Width, Bounds.Height)));   
-        ThePanel.Arrange(new Rect(size));
-
         renderTargetBitmap.Render(ThePanel);
-
-        //renderTargetBitmap.Save("MyFile.png");
-
-        //ImageBrush imageBrush = new ImageBrush(renderTargetBitmap);
-        //TheImage.Background = imageBrush;
 
         WriteableBitmap writeableBitmap = new WriteableBitmap(pixSize, dpi);
         var buff = writeableBitmap.Lock();
 
-        //Bitmap bitmap = renderTargetBitmap.CreateScaledBitmap(pixSize, BitmapInterpolationMode.HighQuality);
         renderTargetBitmap.CopyPixels
         (
             new PixelRect(new PixelPoint(0, 0), pixSize), 
@@ -47,20 +37,17 @@ public partial class MainView : UserControl
             sizeof(int) * buff.Size.Width * buff.Size.Height, 
             buff.RowBytes);
 
-        //writeableBitmap.Save("MyFile1.png");
-
-        Bitmap b = new Bitmap(renderTargetBitmap.Format.Value, AlphaFormat.Premul, buff.Address, pixSize, dpi, buff.RowBytes);
-
-        //b.Save("MyFile2.png");
+        Bitmap b = 
+            new Bitmap
+            (
+                renderTargetBitmap.Format.Value, 
+                AlphaFormat.Premul, 
+                buff.Address, 
+                pixSize, 
+                dpi, 
+                buff.RowBytes);
 
         TheImage.Stretch = Stretch.None;
         TheImage.Source = b;
-    }
-
-    private void MainView_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-    {
-
-
-
     }
 }
