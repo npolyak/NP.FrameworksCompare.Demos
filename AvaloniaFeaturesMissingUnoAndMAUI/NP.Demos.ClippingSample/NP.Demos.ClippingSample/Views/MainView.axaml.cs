25fe54e1162dff7ce1882ade105364f9ad89aebd
npolyak <nick.polyak@verizon.net>
2023-08-17 20:48:15 -0400

    added clipping/opacitymask sample

---
+++ AvaloniaFeaturesMissingUnoAndMAUI/NP.Demos.ClippingSample/NP.Demos.ClippingSample/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace NP.Demos.ClippingSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
