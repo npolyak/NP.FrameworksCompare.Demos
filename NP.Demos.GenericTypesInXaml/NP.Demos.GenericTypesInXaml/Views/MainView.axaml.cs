62f596af3b9ae8179aab2ff39f817a8ddc64a65e
npolyak <nick.polyak@verizon.net>
2023-08-20 13:28:26 -0400

    updated blur sample

---
+++ NP.Demos.GenericTypesInXaml/NP.Demos.GenericTypesInXaml/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace NP.Demos.GenericTypesInXaml.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
